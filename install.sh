075cfeef7df01482f61964cded4fd2b9ce82d821
Chai Feng <chaifeng@chaifeng.com>
2019-04-13 14:04:19 +0800

    Ansible 2.8.0a1

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+ansible_version="2.8.0a1"
+
+if ! ansible --version | grep -F "ansible $ansible_version"; then
+  sudo apt update
+  sudo apt install -y python3 python3-pip
+  pip3 install ansible=="$ansible_version"
+fi
+
+ansible-playbook -l localhost ./devbox.yml
